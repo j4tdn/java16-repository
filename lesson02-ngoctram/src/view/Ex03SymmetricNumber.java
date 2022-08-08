4e948687dd3cebdfef4a0df1bdf11c5769b134b0
jasmineChamCham <jasminebkdn@gmail.com>
2022-08-09 01:31:58 +0700

    lesson02-ngoctram 09.08.2022

---
+++ lesson02-ngoctram/src/view/Ex03SymmetricNumber.java
@@ -0,0 +1,25 @@
+package view;
+
+import java.util.Scanner;
+
+public class Ex03SymmetricNumber {
+	private static boolean isSymmetricNumber(Integer number) {
+		char[] numberToChar = number.toString().toCharArray();
+		int i = 0, n = numberToChar.length;
+		while (i < n / 2) {
+			if (numberToChar[i] == numberToChar[n-1-i])	i++;
+			else return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Integer number = sc.nextInt();
+		if (isSymmetricNumber(number)) 
+			System.out.println(number + " is symmetric");
+		else 
+			System.out.println(number + " is not symmetric");
+		sc.close();
+	}
+}
