a12e9d4a64865b4207ca114f8992fbaf7b0a9660
Quyen Phan <Quyen.Phan@mgm-tp.com>
2022-08-08 15:57:20 +0700

    lesson02-data-structure 07.08.2022

---
+++ lesson02-data-structure/src/condition/loop/Ex01If.java
@@ -0,0 +1,17 @@
+package condition.loop;
+
+public class Ex01If {
+	public static void main(String[] args) {
+		int number = 7;
+		if (isEven(number)) {
+			// statement
+			System.out.println(number + " is even number !!!");
+		}
+		
+		System.out.println("Finish ...");
+	}
+	
+	private static boolean isEven(int number) {
+		return number % 2 == 0;
+	}
+}
---
+++ lesson02-data-structure/src/condition/loop/Ex04SwitchCaseWithoutBreak.java
@@ -0,0 +1,29 @@
+package condition.loop;
+
+import java.util.Random;
+
+public class Ex04SwitchCaseWithoutBreak {
+	public static void main(String[] args) {
+		int month = 1 + new Random().nextInt(12);
+		System.out.println("Tháng " + month + " có ");
+		switch (month) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			System.out.println("31 ngày");
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			System.out.println("30 ngày");
+			break;
+		case 2:
+			System.out.println("28 ngày");
+		}
+	}
+}
