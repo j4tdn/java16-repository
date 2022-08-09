5fa4ce689d896d855fcca1ad76f2034d42022873
tthinh <nguyenminhthethinh@gmail.com>
2022-08-09 12:47:18 +0700

    lesson-02-data-structure 09.08.2022

--- lesson02-data-structure/src/view/Ex01TestMainMethod.java
+++ lesson02-data-structure/src/view/Ex01TestMainMethod.java
@@ -1,21 +1,21 @@
 package view;
 
-	//running: f11
-	//debugging:
-	//breakpoint
+//running: f11
+//debugging:
+//breakpoint
 
 public class Ex01TestMainMethod {
-	//data structure: attributes, variables
-	//algorithm: function, method
+	// data structure: attributes, variables
+	// algorithm: function, method
 	public static void main(String[] args) {
 		System.out.println("Main method");
 		anotherMainMethod();
 	}
-	
-	//chia nho bai toan
-	//tai su dung code
-	
-	//declaration: khai bao
+
+	// chia nho bai toan
+	// tai su dung code
+
+	// declaration: khai bao
 	private static void anotherMainMethod() {
 		System.out.println("Fake main method");
 	}
