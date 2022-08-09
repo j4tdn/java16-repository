5b476bc145b220b620823434c34b769efba82fe4
thuhien <thuhienms2272000@gmail.com>
2022-08-09 14:33:24 +0700

    lesson02-data-structure 09.08.2022

---
+++ lesson02-data-stucture/src/datastructure/Ex13BasicArray.java
@@ -0,0 +1,29 @@
+package datastructure;
+
+public class Ex13BasicArray {
+	public static void main(String[] args) {
+		//mảng là kiểu dữ liệu đối tượng
+		// 5 là độ dài length của mảng
+		//Tạo 1 ô nhớ ở vùng nhớ HEAP lưa 5 giá trị mặc định của kiểu int
+		int [] digits = new int [5];
+		System.out.println("digits[0]="+digits [0]);
+		System.out.println("digits[2]="+digits [0]);
+	digits[1]=99;
+	System.out.println("digits[1]="+digits [1]);
+	//Khởi tạo ô nhớ và gán giá trị cho mảng
+	String [] sequences = {"a","b","c","d"};
+	System.out.println("length:"+sequences.length);
+	//for-index[0-length)
+	for (int i =0;i<=sequences.length;i++);{
+	int i = 0;
+	System.out.println("value:"+sequences[i]);
+	System.out.println("============");
+	for(String each:sequences) {
+		System.out.println("each:"+each);
+	}
+	//Từ khoá break, continue trong vòng lặp
+	}
+	
+	}
+	
+}
