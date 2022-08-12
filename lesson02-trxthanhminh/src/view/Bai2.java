97bd17e08cfc09120fe5b8ef4847168b3ee43b99
txtminh <hiimminh179@gmail.com>
2022-08-12 21:41:48 +0700

    lesson02-trxthanhminh-12-08-2022

---
+++ lesson02-trxthanhminh/src/view/Bai2.java
@@ -0,0 +1,41 @@
+package view;
+
+import java.util.Random;
+
+public class Bai2 {
+	public static void main(String[] args) {
+		Random rd = new Random();
+		int nb1 = rd.nextInt(10);
+		System.out.println("Random: " + nb1);
+		Random ab = new Random();
+		int nb2 = ab.nextInt(10);
+		System.out.println("Random: " + nb2);
+		Random bc = new Random();
+		int nb3 = bc.nextInt(10);
+		System.out.println("Random: " + nb3);
+		Random cd = new Random();
+		int nb4 = cd.nextInt(10);
+		System.out.println("Random: " + nb4);
+		giaithua(nb1);
+		giaithua(nb2);
+		giaithua(nb3);
+		giaithua(nb4);
+		double sum = 0.0f;
+		
+		sum = giaithua(nb1) + giaithua(nb2) + giaithua(nb3) + giaithua(nb4);
+		System.out.println("Result: " + sum);
+			
+		}
+		
+
+	public static double giaithua(int n) {
+	double tmp = 1;
+		if (n > 1) {
+		for (int i = 2; i <= n; i++) {
+		tmp *= i;
+		}
+		}
+		return tmp;
+		}
+	
+}
