e180010dbb4c71a570e555aaf1681449ff102e04
Duc_Trung <kudoshinichi531@gmail.com>
2022-08-12 12:39:10 +0700

    lesson02-haductrung 12.08.2022

---
+++ lesson02-haductrung/src/view/Ex06.java
@@ -0,0 +1,37 @@
+package view;
+
+import java.util.Scanner;
+
+public class Ex06 {
+	public static void main(String[] args) {
+        Scanner number=new Scanner(System.in);
+        System.out.print("enter your number " );
+        int n=number.nextInt();
+        int count = 0;
+        int i=2;
+		while(count<n) {
+			if(isPrimeNumber(i)) {
+				count++;
+				}i++;
+			}                System.out.println(i-1);
+			
+		}
+		
+		
+	
+	public static boolean isPrimeNumber(int n) {    
+        if (n < 2) {
+            return false;
+        }
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+	
+
+}
