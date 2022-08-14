6e64c54521a6a756ae85994345bb9a15474edb19
thanhnhat <lethanhnhat2010@gmail.com>
2022-08-14 15:15:12 +0700

    lesson02-data-structure 14.08.2022

--- lesson02-thanhnhat/src/homework/Ex01Exponentiation.java
+++ lesson02-thanhnhat/src/homework/Ex01Exponentiation.java
@@ -2,21 +2,32 @@ package homework;
 
 public class Ex01Exponentiation {
 	public static void main(String[] args) {
-		int b = 7;
-		if (isPowerOfTwo(b)) {
-			System.out.println("a là luỹ thừa của 2");
-		}
-		else
-		System.out.println("a không là luỹ thừa của 2");
-	}
+		int[] numbers = { 2, 0, 4, 8, 12, 66, 45, 128, 100 };
+		for (int number : numbers) {
+			// 1st way
+			System.out.println(number + " => " + isPowerOfTwo(number));
+			// System.out.println(b + " => " + isPowerOf(2,n));
 
-	public static boolean isPowerOfTwo(int n) {
-		if (0 == n || 1 == n)
-			return true;
-		int x = n / 2;
-		int y = n % 2;
-		if (1 == y) return false;
-		return isPowerOfTwo(x);
- 
+		}
+
+	}
+	/*
+	 * // 2nd way //* b is power of a ==> a^n = b ==> conditon (b>0, n is integer)
+	 * private static boolean isPowerOf(int a, int b) { if (b<=0) { return false; }
+	 * double n = Math.log(b)/Math.log(a); return Math.ceil(n) == Math.floor(n); }
+	 */
+
+	private static boolean isPowerOfTwo(int number) {
+		if (number <= 0) {
+			return false;
+		}
+		while (number != 1) {
+			if (number % 2 != 0) {
+				return false;
+			}
+			number /= 2;
+		}
+		return true;
+
 	}
 }
