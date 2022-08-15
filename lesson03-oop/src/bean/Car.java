87d31362bc1a7aa8d402ed53c71fee6d6a713efc
Quyen Phan <Quyen.Phan@mgm-tp.com>
2022-08-15 22:05:28 +0700

    lesson03-oop 15.08.2022

---
+++ lesson03-oop/src/bean/Car.java
@@ -0,0 +1,25 @@
+package bean;
+
+public class Car {
+	
+	// object's scope
+	public int serial;
+	public String color;
+	
+	// class's scope
+	// all objects have the same value
+	public static String model;
+	
+	public Car() {
+	}
+
+	public Car(int serial, String color) {
+		this.serial = serial;
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [serial=" + serial + ", model=" + model + ", color=" + color + "]";
+	}
+}
