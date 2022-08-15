3c8c46033d533dc8fc52e14b5b02846727a531df
tthinh <nguyenminhthethinh@gmail.com>
2022-08-15 19:34:13 +0700

    lesson02-tthinh 15.08.2022

--- lesson02-tthinh/src/view/Ex02.java
+++ lesson02-tthinh/src/view/Ex02.java
@@ -4,30 +4,27 @@ import java.util.Random;
 
 public class Ex02 {
 	public static void main(String[] args) {
-		int[] number = new int[4];
-		int arbitraryNumber = new Random().nextInt(1,10);
-		System.out.println("Number 0: " + arbitraryNumber);
-		int arbitraryNumber1 = new Random().nextInt(1,10);
-		System.out.println("Number 1: " + arbitraryNumber1);
-		int arbitraryNumber2 = new Random().nextInt(1,10);
-		System.out.println("Number 2: " + arbitraryNumber2);
-		int arbitraryNumber3 = new Random().nextInt(1,10);
-		System.out.println("Number 3: " + arbitraryNumber3);
-		System.out.println(sum(number));
+		System.out.println(sumOfFactorial(new int[] {5}));
+		System.out.println(sumOfFactorial(4,5));
+		System.out.println(sumOfFactorial(4,5,7));
+		System.out.println(sumOfFactorial());
+		
 	}
 
-	private static int sum(int[] numbers) {
+	private static int factorial(int n) {
+		int result = n;
+		while(n>1) {
+			result *= --n;
+		}
+		return result;
+	}
+
+	//using variables ... instead of array in some case
+	private static int sumOfFactorial(int... numbers ) {
 		int sum = 0;
-		for (int number : numbers) {
+		for (int number: numbers) {
 			sum += factorial(number);
 		}
 		return sum;
-	}
-
-	private static int factorial(int number) {
-		int fac = 1;
-		for (int i = 1; i <= number; i++)
-			fac *= i;
-		return fac;
 	}
 }
--- lesson02-tthinh/src/view/Ex04.java
+++ lesson02-tthinh/src/view/Ex04.java
@@ -4,36 +4,27 @@ import java.util.Scanner;
 
 public class Ex04 {
 	public static void main(String[] args) {
-		Scanner ip = new Scanner(System.in);
-		int n;
-		do {
-			System.out.println("Enter your array number: ");
-			n = Integer.parseInt(ip.nextLine());
-		} while (n <= 0);
-
-		int[] digits = new int[n];
-
-		System.out.println("===Enter your number===");
-		for (int i = 0; i < n; i++) {
-			System.out.println("Enter your number " + (i + 1) + " : ");
-			digits[i] = Integer.parseInt(ip.nextLine());
-		}
-		System.out.println("======Your number=====");
-		for (Integer items : digits) {
-			System.out.println(items);
-		}
-		int max = 0;
-		for (int i = 0; i < n; i++) {
-			if (checkOddNumber(digits[i]) == true) {
-				if (max < digits[i]) {
-					max = digits[i];
-				}
-			}
-		}
-		System.out.println("Max even number in array " + max);
+		int[] numbers = { 50, 10, 12, 30 };
+		System.out.println("max odd: " + findMaxOddElement(numbers));
 	}
 
-	public static boolean checkOddNumber(int n) {
-		return (n % 2 != 0);
+	private static int findMaxOddElement(int[] elements) {
+		int max = Integer.MIN_VALUE;
+		int countOdd = 0;
+		for (int element : elements) {
+			if (isOdd(element) && element > max) {
+				max = element;
+				countOdd++;
+			}
+		}
+		if (countOdd == 0) {
+			System.out.println("Array has no odd element!!!");
+			System.exit(0);
+		}
+		return max;
+	}
+
+	private static boolean isOdd(int number) {
+		return number % 2 != 0;
 	}
 }
