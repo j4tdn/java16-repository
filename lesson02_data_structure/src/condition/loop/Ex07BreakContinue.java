fef242b5d1a1ac7a27db4df3cad04fb9137153f9
buivu <buianhvu226@gmail.com>
2022-08-17 00:18:38 +0700

    lesson02-buianhvu 15.08.2022

--- lesson02_data_structure/src/condition/loop/Ex07BreakContinue.java
+++
@@ -1,7 +0,0 @@
-package condition.loop;
-
-public class Ex07BreakContinue {
-	// trong cấu trúc vòng lặp(for,while,do while)
-	//break -->thoát khởi vòng lặp ngay lập tức.
-	
-}
--- lesson03-oop/src/bean/Car.java
+++ lesson03-oop/src/bean/Car.java
@@ -2,30 +2,25 @@ package bean;
 
 public class Car {
 	//object's scope
-	public int  serial;
+	public int serial;
 	public String color;
-	
 	//class's scope
 	public static String model;
 	
-	
-	public Car() {
-		// TODO Auto-generated constructor stub
+	public  Car() {
+		
 	}
-	
+
 	public Car(int serial, String color) {
-	
-		this.color = color;
 		this.serial = serial;
+		this.color = color;
+	}
+	public Car(int i, String string, String string2) {
+		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public String toString() {
-		return "Car [model=" + model + ", color=" + color + ", serial=" + serial + "]";
+		return "Car [serial=" + serial + ", model=" + model + ", color=" + color + "]";
 	}
-
-	
-	
-
-	
 }
