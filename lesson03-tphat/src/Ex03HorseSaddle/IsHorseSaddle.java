55da00380493515f5e8f8c7c510dc61683f2132a
HuynhTanPhat <huynhtanphat.24112001@gmail.com>
2022-08-18 17:39:26 +0700

    lesson03-tphat 18.08.2022

--- lesson03-tphat/src/Ex03HorseSaddle/IsHorseSaddle.java
+++ lesson03-tphat/src/Ex03HorseSaddle/IsHorseSaddle.java
@@ -6,18 +6,18 @@ import java.util.Scanner;
 public class IsHorseSaddle {
 
 	public static void main(String[] args) {
-//		Scanner input = new Scanner(System.in);
-//		System.out.print("Nhập số dòng: ");
-//		int row = input.nextInt();
-//		System.out.print("Nhập số cột: ");
-//		int col = input.nextInt();
-//		int a[][] = new int[row][col];
-//		NhapMaTran(a, row, col);
-//		XuatMaTran(a, row, col);
-//		System.out.println();
-//		findSaddlePoint(a);
-		int[][] a = { { 3, 3, 4 }, { 3, 3, 4 }, { 2, 3, 3 } };
-		findSaddlePoint(a, 3, 2);
+//			Scanner input = new Scanner(System.in);
+//			System.out.print("Nhập số dòng: ");
+//			int row = input.nextInt();
+//			System.out.print("Nhập số cột: ");
+//			int col = input.nextInt();
+//			int a[][] = new int[row][col];
+//			NhapMaTran(a, row, col);
+//			XuatMaTran(a, row, col);
+//			System.out.println();
+//			findSaddlePoint(a);
+		int[][] a = { { 10, 3, 11 }, { 10, 10, 10 } };
+		findSaddlePoint(a, 2, 3);
 	}
 
 	public static void NhapMaTran(int a[][], int row, int col) {
@@ -36,26 +36,33 @@ public class IsHorseSaddle {
 
 	}
 
+	public static int isMin(int[][] a, int indexRow, int col) {
+		int min = Integer.MAX_VALUE;
+		for (int x = 0; x < col; x++) {
+			if (a[indexRow][x] <= min) {
+				min = a[indexRow][x];
+			}
+		}
+		return min;
+	}
+
 	static void findSaddlePoint(int[][] a, int row, int col) {
 		int colIndex;
 		int max;
 		for (int i = 0; i < row; i++) {
-			int min = a[i][0];
 			for (int j = 0; j < col; j++) {
-				if (a[i][j] <= min) {
-					min = a[i][j];
+				if (a[i][j] <= isMin(a, i, col)) {
 					max = a[i][j];
-					for (int k = 0; k < col; k++) {
+					for (int k = 0; k < row; k++) {
 						if (max < a[k][j]) {
 							max = a[k][j];
 						}
 					}
-					if (max == min) {
-						System.out.println("Điểm yên ngựa: " + min + " ở hàng " + (i + 1) + " cột " + (j + 1));
+					if (max == isMin(a, i, col)) {
+						System.out.println("Điểm yên ngựa: " + a[i][j] + " ở hàng " + (i + 1) + " cột " + (j + 1));
 					}
 				}
 			}
 		}
 	}
 }
-
