2097ff1ca62913706ab866298987a3e13f2f4129
= <linh1862002@gmail.com>
2022-08-19 19:36:52 +0700

    lesson3-duclinh 19.08.2022

---
+++ lesson03-duclinh/src/vehicle/VehicleUtils.java
@@ -0,0 +1,15 @@
+package vehicle;
+
+public class VehicleUtils {
+	private  VehicleUtils() {
+		
+	}
+	public static double payTax(Vehicle vehicle) {
+		double tax;
+		int capacity = vehicle.getCapacity();
+		if(capacity < 100) tax = vehicle.getCost() * 0.1; 
+		else if(capacity >= 100 && capacity <= 200) tax = vehicle.getCost()*0.3;
+		else tax =vehicle.getCost() * 0.5;
+		return tax;
+	}
+}
