ea1f5ab691604dd327a865e0a9f00bb988ff85d8
Duc_Trung <kudoshinichi531@gmail.com>
2022-08-19 22:00:21 +0700

    lesson03-haductrung 19.08.2022

---
+++ lesson03-haductrung/src/saddle/HorseSaddle.java
@@ -0,0 +1,23 @@
+package saddle;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class HorseSaddle {
+	public static void main(String[] args) {
+		int m = 0;
+		int n = 0;
+		Scanner ip = new Scanner(System.in);
+		System.out.println("Nhap so hang : ");
+		m = ip.nextInt();
+		System.out.println("Nhap so cot: ");
+		n = ip.nextInt();
+		int[][] A = new int[m][n];
+
+
+
+	}
+
+	
+
+}
---
+++ lesson03-haductrung/src/shopping/OrderUltils.java
@@ -0,0 +1,23 @@
+package shopping;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class OrderUltils {
+	public static double export(Order order) {
+		double totalOfMoney = 0;
+
+		ItemDetail[] ids = order.getItemDetail();
+		for (ItemDetail id : ids) {
+			Item item = id.getItem();
+			int quantity = id.getQuantity();
+			double idCost = item.getCost() * quantity;
+			if (item.getCost() > 590 && LocalDate.of(20022, Month.AUGUST, 19).isEqual(order.getOrderDate().toLocalDate())) {
+				idCost *= 0.9; 
+			}
+			totalOfMoney += idCost;
+		}
+		return totalOfMoney;
+	}
+
+}
