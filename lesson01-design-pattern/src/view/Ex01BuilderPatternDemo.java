d334109ecf2114f1850035d87663b76f56d30982
Doan Vu Duy <doanvuduyndh@gmail.com>
2022-08-21 04:28:00 +0700

    lesson3-oop 21.08.2022

---
+++ lesson01-design-pattern/src/view/Ex01BuilderPatternDemo.java
@@ -0,0 +1,27 @@
+package view;
+
+import bean.User;
+import bean.UserDetail;
+
+/*
+ * Đặt vấn đề 
+ * -->Builder pattern được sử dụng để tạo ra đối tượng phức tạp.
+ * -->Phức tạp: nhiều thuộc tính.
+ * 1. Dễ xảy ra lỗi khi truyền không đúng thứ tự tham số cho đối tượng
+ * 2. Khi yêu cầu tạo ra  quá nhiều hàm khởi tạo, un-clean code.
+ */
+public class Ex01BuilderPatternDemo {
+	public static void main(String[] args) {
+		User uA = User.builder().withLastName("teo").withLastName("le")
+
+				.withPhone("0384396351").build();
+		System.out.println(uA);
+
+		User uB = User.builder().withLastName("hoang").withLastName("doan").withPhone("0384396351").build();
+		System.out.println(uB);
+
+		UserDetail uC = new UserDetail().withAge(22).withPhone("0123456689");
+		System.out.println(uC);
+
+	}
+}
