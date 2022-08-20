e52c1f69e874b1414b897697e998562f167f8687
thuhien <thuhienms2272000@gmail.com>
2022-08-20 14:38:53 +0700

    lesson03-thuhien 19.08.2022

---
+++ lesson03-baitap/src/shopping/OderUtils.java
@@ -0,0 +1,31 @@
+package shopping;
+import java.time.LocalDate;
+import java.time.Month;
+
+/*
+ * Utility of Oder Class
+ * Normally: Utility is place where contains utility methods
+ * No cares created an object 	
+ */
+public class OderUtils {
+	private OderUtils() {
+	}
+	
+	public static double export(Order order) {
+		double totalOfMoney = 0;
+		
+		ItemDetail[] ids = order.getItemDetails();
+		for(ItemDetail id:ids) {
+			Item item = id.getItem();
+			int quantity = id.getQuantity();
+			
+			if(item.getCost() > 590 && LocalDate.of(2021, Month.MAY, 8).isEqual(order.getOrderDate().toLocalDate())) {
+				totalOfMoney += item.getCost() * quantity * 0.9;
+			} else {
+				totalOfMoney += item.getCost() * quantity;
+			}
+		}
+		
+		return totalOfMoney;
+	}
+}
