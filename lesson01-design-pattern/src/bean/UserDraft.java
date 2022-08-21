78ee3d1efbc796adf449a2a937cd6b1298abdd6e
hongnhung <nhonho2507@gmail.com>
2022-08-21 15:37:23 +0700

    lesson03-oop 21-08-2022

---
+++ lesson01-design-pattern/src/bean/UserDraft.java
@@ -0,0 +1,73 @@
+package bean;
+
+public class UserDraft {
+	//why need to declare private
+	//mandantory: firstNam, lastName, the rest is optional
+	 private String firstName;
+	 private String lastName;
+	 private int age;
+	 private String phone;
+	 private String address;
+	 
+	 public UserDraft() {
+	}
+
+	public UserDraft(String firstName, String lastName, int age, String phone, String address) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.phone = phone;
+		this.address = address;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "User [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", phone=" + phone
+				+ ", address=" + address + "]";
+	}
+	 
+	 
+
+
+}
