599761005126574d172a6dfb354ed2620dc46772
quocthinh <quocthinhtran2611@gmail.com>
2022-08-28 22:01:55 +0700

    lesson04-oop-feature 28.08.2022

---
+++ lesson04-oop-features/src/polimophism/object/Shape.java
@@ -0,0 +1,11 @@
+package polimophism.object;
+
+public class Shape {
+	void draw() {
+		System.out.println("Shape --> draw ... ");
+	}
+	
+	void calS() {
+		System.out.println("Shape --> cal S ...");
+	}
+}
---
+++ lesson04-oop-features/src/polymorphism/method/Ex02OverrdingParent.java
@@ -0,0 +1,9 @@
+package polymorphism.method;
+
+public class Ex02OverrdingParent {
+	String text = "Hello";
+	
+	void log() {
+		System.out.println("Parent --> log...");
+	}
+}
