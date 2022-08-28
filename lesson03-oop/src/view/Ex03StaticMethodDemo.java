8e96bbcaece85580026a3d5f3ff6a1c98e4e6543
THIENMAP <thienmapad190606@gmail.com>
2022-08-28 13:48:09 +0700

    lesson03-oop 28.08.2022

---
+++ lesson03-oop/src/view/Ex03StaticMethodDemo.java
@@ -0,0 +1,27 @@
+package view;
+
+public class Ex03StaticMethodDemo {
+	private int x;
+	private int y;
+	public Ex03StaticMethodDemo(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public static void main(String[] args) {
+		System.out.println(sum(5,10));
+		Ex03StaticMethodDemo o1 = new Ex03StaticMethodDemo(8,2);
+		System.out.println("sub :" + o1.sub());
+		
+	}
+	// Hàm chỉ phụ thuộc vào tham số truyền vào không phụ thuộc vào đối tượng đang gọi
+	// ==> Cho hàm static 
+	private static int sum(int a, int b) {
+		return a + b ;
+	}
+	// Hàm phụ thuộc vào đối tượng đang gọi
+	// ==> non-static 
+	private int sub() {
+		return x - y;
+	}
+
+}
