dce872d3bf9732ce560ca2dcc1c4858479816930
thuhien <thuhienms2272000@gmail.com>
2022-08-29 19:15:19 +0700

    lesson04-oop features 28.08.2022

---
+++ lesson04-oop-features/src/inheritance/finterface/ShapeDemo.java
@@ -0,0 +1,48 @@
+package inheritance.finterface;
+
+public class ShapeDemo {
+public static void main(String[] args) {
+	Shape rect = new Rectangle();
+	rect.draw();
+	rect.cals();
+	//Shape: interface
+	//Anonymous class: Lớp ẩn danh
+	Shape shape = new Shape() {
+		
+		@Override
+		public void draw() {
+		 System.out.println("X --> draw ...");
+		}
+		
+		@Override
+		public void cals() {
+			System.out.println("X --> cal S");		
+		}
+	};
+}public static void main(String[] args) {
+	Shape rect = new Rectangle();
+	rect.draw();
+	rect.calS();
+
+	// Shape: interface
+	// Anonymous class: Lớp ẩn danh
+	Shape shape = new Shape() {
+
+		@Override
+		public void draw() {
+			System.out.println("X --> draw ...");
+		}
+
+		@Override
+		public void calS() {
+			System.out.println("X --> cal S ...");
+		}
+	};
+	shape.draw();
+	shape.calS();
+
+	Shape shapeX = new X();
+	shapeX.draw();
+	shapeX.calS();
+}
+}
---
+++ lesson04-oop-features/src/polymorphism/object/Square.java
@@ -0,0 +1,16 @@
+package polymorphism.object;
+
+public class Square extends Shape {
+	void draw() {
+		System.out.println("Square --> draw ...");
+	}
+
+	@Override
+	void calS() {
+		System.out.println("Square --> cal S ...");
+	}
+
+	void clear() {
+		System.out.println("Square --> clear ...");
+	}
+}
