d9de748597000b2eef1ef260edb662cb0ceed872
hongnhung <nhonho2507@gmail.com>
2022-08-30 00:13:40 +0700

    lesson04-oop-feature 29-08-2022

---
+++ lesson04-oop-features/src/example/MouseClickEvent.java
@@ -0,0 +1,22 @@
+package example;
+
+public abstract class MouseClickEvent implements MouseEvent{
+
+	@Override
+	public void onMouseEnter() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void onMouseRelease() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void onMouseClick() {
+		// TODO Auto-generated method stub
+		
+	}
+}
