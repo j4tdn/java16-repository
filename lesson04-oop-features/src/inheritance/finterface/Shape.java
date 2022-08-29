626b56445aff762beedf9976c3017d28a936d507
Quyen Phan <Quyen.Phan@mgm-tp.com>
2022-08-29 13:39:20 +0700

    lesson04-oop-features 28.08.2022

---
+++ lesson04-oop-features/src/inheritance/finterface/Shape.java
@@ -0,0 +1,12 @@
+package inheritance.finterface;
+
+public interface Shape {
+	// method: declaration: khai báo
+	//       : implementation: thực thi
+	
+	// JAVA07 trở về trước: 1 hàm trong interface là không có thân hàm
+	// hàm trừu tượng - abstract method
+	void draw();
+	
+	void calS();
+}
