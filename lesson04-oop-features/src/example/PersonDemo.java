c70c722d55a4f2327678b745ea8b301766d8cd29
quocthinh <quocthinhtran2611@gmail.com>
2022-09-02 09:27:07 +0700

    lesson04-oop-feature 28.08.2022

---
+++ lesson04-oop-features/src/example/PersonDemo.java
@@ -0,0 +1,24 @@
+package example;
+
+public class PersonDemo {
+	public static void main(String[] args) {
+		Student stA = new Student() {
+			
+			@Override
+			public void gender() {
+				System.out.println("MALE");
+			}
+		};
+		
+		Employee empB = new Employee() {
+			
+			@Override
+			public void gender() {
+				System.out.println("MALE");
+			}
+		};
+		stA.eat();
+		empB.eat();
+	}
+
+}
