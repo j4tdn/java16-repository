23fb8e5999bd27d930618926de9874aa6dc0ace0
thuhien <thuhienms2272000@gmail.com>
2022-09-05 19:32:13 +0700

    lesson04-thuhien

--- lesson04-thuhien/src/ex04/Director.java
+++ lesson04-thuhien/src/ex04/Director.java
@@ -1,5 +1,31 @@
 package ex04;
+import java.time.LocalDate;
+public class Director extends People {
+	private int coefficientSalary;	
 
-public class Director {
+	public Director() {
+	}
 
+	public Director(String name, LocalDate birthday, int coefficoen, int coefficientSalary) {
+		super(name, birthday, coefficoen);
+		this.coefficientSalary = coefficientSalary;
+	}
+
+	public int getCoefficientSalary() {
+		return coefficientSalary;
+	}
+
+	public void setCoefficientSalary(int coefficientSalary) {
+		this.coefficientSalary = coefficientSalary;
+	}
+
+	@Override
+	public String toString() {
+		return "Director "+super.toString()+" coefficientSalary=" + coefficientSalary + "]";
+	}
+	
+	@Override
+	public float salary() {
+		return (this.getCoefficoen() + this.getCoefficientSalary()) * 300000;
+	}
 }
