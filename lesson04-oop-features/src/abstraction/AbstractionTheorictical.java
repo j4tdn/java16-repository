62e0bc302388250627ddee5ce65609ac7a1b7034
thuhien <thuhienms2272000@gmail.com>
2022-09-05 15:15:29 +0700

    lesson04-oop-features 03.09.2022

---
+++ lesson04-oop-features/src/abstraction/AbstractionTheorictical.java
@@ -0,0 +1,13 @@
+package abstraction;
+
+public class AbstractionTheorictical {
+/*
+ * Tính chất trừu tượng được thể hiện qua 
+ * + Class, Abstract Class, Interface, Abstract Method
+ * 
+ * Vd Khi muốn tạo ra hàm trong KDL, hàm đó chưa có nội dung cụ thể  
+ * ==> Tạo ra hàm trừu tượng trong Abstract class, Interface
+ * 
+ * .. functional Interface, Lambda expression
+ */
+}
---
+++ lesson04-oop-features/src/inheritance/multple/GrandMother.java
@@ -0,0 +1,10 @@
+package inheritance.multple;
+
+public interface GrandMother {
+	default void adn() {
+		System.out.println("System.out.println(\"abcxyz123456\");");
+	}
+
+	void swimming();
+
+}
