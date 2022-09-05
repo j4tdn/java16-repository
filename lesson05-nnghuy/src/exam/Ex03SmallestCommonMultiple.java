44b3835817800dfe8c29c36e229cd2cfe123baa4
Nguyễn Nho Gia Huy <yahwi32@gmail.com>
2022-09-05 21:41:17 +0700

    lesson05-nnghuy 05.09.2022

---
+++ lesson05-nnghuy/src/exam/Ex03SmallestCommonMultiple.java
@@ -0,0 +1,31 @@
+package exam;
+
+import java.util.Scanner;
+
+public class Ex03SmallestCommonMultiple {
+	public static void main(String[] args) {
+		Scanner ip = new Scanner(System.in);
+		System.out.print("Mời nhập n:");
+		int n = ip.nextInt();
+		int[] a = new int[n];
+		for(int i = 0; i<n;i++) {
+			System.out.print("a["+i+"]=");
+			a[i] = ip.nextInt();
+		}
+		
+		System.out.println("BCNN = " + getLeastCommonMultiple(a, n));
+		
+	}
+	
+	private static int getLeastCommonMultiple(int[] a, int n) {
+		int c=(a[0]*a[1])/GCD(a[0], a[1]);
+		for(int i = 2; i<n;i++) {
+			c=(c*a[i])/GCD(c, a[i]);
+		}
+		return c;
+	}
+	private static int GCD(int a, int b) {
+		if (b==0) return a;
+		return  GCD(b, a%b);
+	}
+}
