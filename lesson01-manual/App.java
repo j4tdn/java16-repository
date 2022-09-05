a407acdb8ea074de83656f3e98dc69852e9ab770
yahwi <106938614+yahwi32@users.noreply.github.com>
2022-09-05 11:14:10 +0700

    Delete lesson01-manual directory

--- lesson01-manual/App.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDate;
-
-public class App {
-   public static void main(String[] args) {
-	System.out.println("Welcom to JAVA16 class:");
-	System.out.println("Publish date: " + LocalDate.now());
-   }	
-}
--- lesson01-manual/src/App.java
+++
@@ -1,7 +0,0 @@
-import java.time.LocalDate;
-public class App {
-	public static void main(String[] args) {
-	      System.out.println("Welcome to JAVA16 class");
-	      System.out.println("Publish date: " + LocalDate.now());
-	   }
-}
