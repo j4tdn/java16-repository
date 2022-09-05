636f857106a4a6b01d3218839162a93aa6d9893a
TranHuy <tranhuy1st113@gmail.com>
2022-09-05 21:46:48 +0700

    lesson05-tkhuy 05.09.2022

---
+++ lesson05-tkhuy/src/ex01/PowerOf.java
@@ -0,0 +1,15 @@
+package ex01;
+import java.lang.Math;
+public class PowerOf {
+	public static void main(String[] args) {
+		System.out.println("8-2 " + isPowerOf2(8, 2));
+		System.out.println("7-3 " + isPowerOf2(7, 3));
+		System.out.println("2-8 " + isPowerOf2(2, 8));
+	}
+	private static boolean isPowerOf2(int a, int b) {
+		//Math.log(X)  logarit
+		double log = (a > b) ? Math.log(a) / Math.log(b): Math.log(b) / Math.log(a);
+		return Math.ceil(log) == Math.floor(log);
+		
+	}
+}
