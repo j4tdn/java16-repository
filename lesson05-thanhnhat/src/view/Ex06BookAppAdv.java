093e43e69a9deaea00bf35282f823a34584a3aff
thanhnhat <lethanhnhat2010@gmail.com>
2022-09-15 23:09:12 +0700

    lesson05-thanhnhat 12.09.2022

--- lesson05-thanhnhat/src/view/Ex06BookAppAdv.java
+++ lesson05-thanhnhat/src/view/Ex06BookAppAdv.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.util.Arrays;
+import java.util.concurrent.locks.Condition;
 
 import bean.Book;
 import bean.ClassicalBook;
@@ -10,53 +11,51 @@ public class Ex06BookAppAdv {
 	public static void main(String[] args) {
 		
 		Book[] books = initial();
+		// Anonymous class => Tạo đối tượng cho interface
+		BookCondition cd = new BookCondition() {
+			
+			@Override
+			public boolean test(Book book) {
+				return "Nhi Đồng".equals(book.getPublisher());
+			}
+		};
 		
-		
-		System.out.println("============");
-		
-		
-		
-		Book[] booksByPublisher = findBooks(books, book -> "Sao mai".equals(book.getPublisher()));
+		// lambda expression
+	
+		Book[] booksByPublisher = findbooks(books, book -> "Nhi Đồng".equals(book.getPublisher()));
 		printf(booksByPublisher);
-		
-		System.out.println("============");
-		
-		Book[] booksByLtPrice = findBooks(books, book ->book.getPrice() >50);
+
+		System.out.println("=========");
+		Book[] booksByLtPrice = findbooks(books, book -> book.getPrice()<50);
 		printf(booksByLtPrice);
+		
 	}
+
+	private static Book[] findbooks(Book[] books, BookCondition condition) {
+		Book[] result = new Book[books.length];
+		int k = 0;
+		for (Book book : books) {
+			if (condition.test(book)) {
+				result[k++] = book;
+			}
+		}
+
+		return Arrays.copyOfRange(result, 0, k);
+	}
+
 	private static Book[] initial() {
-		return new Book[] { new ClassicalBook("SGK1", 12d, "Nhi đồng", false),
-				new ClassicalBook("SGK2", 68d, "Lạc việt", true), new ClassicalBook("SGK3", 55d, "Lạc hồng", false),
-				new ClassicalBook("SGK4", 46d, "Nhi đồng", true), new ReferenceBook("STK1", 44d, "Nhi đồng", 5),
-				new ReferenceBook("STK2", 61d, "Sao mai", 7), new ReferenceBook("STK3", 33d, "Lạc việt", 8) };
+		return new Book[] { new ClassicalBook("SGK1", 44d, "Nhi Đồng", true),
+				new ClassicalBook("SGK2", 65d, "Sao Mai", false), new ClassicalBook("SGK3", 12d, "Lạc Việt", true),
+				new ClassicalBook("SGK4", 108d, "Nhi Đồng", true), new ReferenceBook("STK1", 66, "Nhi Đồng", 4),
+				new ReferenceBook("STK2", 22, "Lạc Việt", 2), new ReferenceBook("STK3", 33, "Sao Mai", 5),
+				new ReferenceBook("STK4", 45, "Nhi Đồng", 3)
+
+		};
 	}
 
 	private static void printf(Book[] books) {
 		for (Book book : books) {
 			System.out.println(book);
 		}
-	}
-	// strategy pattern
-	private static Book[] findBooks(Book[] books, BookConditon condition) {
-		Book[] result = new Book[books.length];
-		int k = 0;
-		for (Book book : books) {
-			if (condition.text(book)) {
-				result[k++] = book;
-			}
-		}
-		return Arrays.copyOfRange(result, 0, k);
-	}
-
-//Tìm sách NXB Nhi Đồng
-	private static Book[] findBooks(Book[] books, String publisher) {
-		Book[] result = new Book[books.length];
-		int k = 0;
-		for (Book book : books) {
-			if (publisher.equals(book.getPublisher())) {
-				result[k++] = book;
-			}
-		}
-		return Arrays.copyOfRange(result, 0, k);
 	}
 }
