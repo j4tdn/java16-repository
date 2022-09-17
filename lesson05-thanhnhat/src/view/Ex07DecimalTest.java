1409c94137089feef41c6d8015409bc82e737e5d
thanhnhat <lethanhnhat2010@gmail.com>
2022-09-18 01:51:55 +0700

    lesson05-thanhnhat 12.09.2022

--- lesson05-thanhnhat/src/view/Ex07DecimalTest.java
+++ lesson05-thanhnhat/src/view/Ex07DecimalTest.java
@@ -6,8 +6,38 @@ public class Ex07DecimalTest {
 		// 0.1 -> 1/10
 		// 0.2 -> 2/10
 		// 1.0 -> 10/10
-		// 12.5 -> 125/100
-		
+		// 12.5 -> 125/10
+		  double d = 0.05d;
+			System.out.println(division(d));
+			
+			System.out.println(glc(int denominator, int numerator));
 	}
+	public static String division(double d){
+        String result="";
+        String dString = Double.toString(d);        
+        String[] fraction = dString.split("\\.");
 
+        int denominator = (int)Math.pow(10, fraction[1].length());
+        int numerator = Integer.parseInt(fraction[0] + "" + fraction[1]);
+       
+        result = numerator + "/" + denominator;
+        
+        return result;
+    }
+	private static String glc(int denominator, int numerator) {
+	 while (denominator != numerator) {
+			if (denominator > numerator) {
+				denominator = denominator - numerator;
+			} else {
+				numerator = numerator - denominator;
+			}
+		} 
+     for (int i = 2; i <= 9; i++) {
+    	if (denominator % i == 0 && numerator  % i == 0) {
+    		denominator = denominator/i;
+    		numerator = numerator/i;
+    	}
+     }
+     return numerator + "/" + denominator;
+	}
 }
