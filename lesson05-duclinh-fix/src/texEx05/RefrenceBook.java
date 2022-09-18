a0725d47aeb70d20e5458551ba061cb22cfe37cd
= <linh1862002@gmail.com>
2022-09-18 13:54:09 +0700

    12.9.2022

---
+++ lesson05-duclinh-fix/src/texEx05/RefrenceBook.java
@@ -0,0 +1,21 @@
+package texEx05;
+
+public class RefrenceBook extends Book{
+	private double tax;
+	public RefrenceBook() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public RefrenceBook(String id, double price, String publiser, double tax) {
+		super(id, price, publiser);
+		this.tax = tax;
+	}
+
+	public double getTax() {
+		return tax;
+	}
+	public void setTax(double tax) {
+		this.tax = tax;
+	}
+	
+}
