4b408b57039d0a6c63d4af009f599f7a3b2e85f3
tthinh <nguyenminhthethinh@gmail.com>
2022-09-29 09:18:19 +0700

    lesson07-libraries 29.09.2022

--- lesson01-design-pattern/src/bean/User.java
+++ lesson01-design-pattern/src/bean/User.java
@@ -1,22 +1,24 @@
 package bean;
 
 public class User {
+	// why we need to declare private
+	// mandatory: firstName, lastName, the rest is optional
 	private String firstName;
 	private String lastName;
 	private int age;
 	private String phone;
 	private String address;
 
-	public User(Builder builder) {
+	private User(Buider builder) {
 		this.firstName = builder.firstName;
 		this.lastName = builder.lastName;
-		this.age = age;
-		this.phone = phone;
-		this.address = address;
+		this.age = builder.age;
+		this.phone = builder.phone;
+		this.address = builder.address;
 	}
-
-	public static Builder builder() {
-		return new Builder();
+	
+	public static Buider builder() {
+		return new Buider();
 	}
 
 	// getter
@@ -47,42 +49,47 @@ public class User {
 				+ ", address=" + address + "]";
 	}
 
-	// nasted class
-	public static class Builder {
+	// nested class
+
+	public static class Buider {
 		private String firstName;
 		private String lastName;
 		private int age;
 		private String phone;
 		private String address;
 
+		private Buider() {
+
+		}
+
 		// setter
-		public Builder withFirstName(String firstName) {
+		public Buider withFirstName(String firstName) {
 			this.firstName = firstName;
 			return this;
 		}
 
-		public Builder withLastName(String lastName) {
-			this.firstName = lastName;
+		public Buider withLastName(String lastName) {
+			this.lastName = lastName;
 			return this;
 		}
 
-		public Builder withAge(int age) {
+		public Buider withAge(int age) {
 			this.age = age;
 			return this;
 		}
 
-		public Builder withPhone(String phone) {
+		public Buider withPhone(String phone) {
 			this.phone = phone;
 			return this;
 		}
 
-		public Builder withAdress(String Adress) {
+		public Buider withAddress(String address) {
 			this.address = address;
 			return this;
 		}
 
 		public User build() {
-			// this = current Builder xem lai
+			// this = current Builder
 			return new User(this);
 		}
 	}
