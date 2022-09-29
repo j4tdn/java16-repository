53296615b59d96001acb1218f1bcf7c0d928afea
AnhTuan <ptuan8674@gmail.com>
2022-09-29 15:43:37 +0700

    lesson06-anhtuan 29.09.2022

--- lesson06-anhtuan/src/view/Ex04.java
+++ lesson06-anhtuan/src/view/Ex04.java
@@ -10,13 +10,19 @@ public class Ex04 {
 				int n = sc.nextInt();
 				String s = String.valueOf(n);
 				if(s.length() == 1) {
-					System.out.println(n + " doc la " + oneNumber(n));
+					System.out.println(n + " --> " + oneNumber(n));
 				}
 				if(s.length() == 2) {
-					System.out.println(n + " doc la " + read2Number(n));
+					System.out.println(n + " --> " + read2Number(n));
 				}
 				if(s.length() == 3) {
-					System.out.println(n + " doc la " + read3Number(n));
+					System.out.println(n + " --> " + read3Number(n));
+				}
+				if(s.length() >= 4 && s.length() <= 6) {
+					System.out.println(read4Num(n));
+				}
+				if(s.length() >= 7 && s.length() <= 9) {
+					System.out.println(read6Num(n));
 				}
 			}while(true);
 		}
@@ -26,32 +32,38 @@ public class Ex04 {
 	}
 	private static String oneNumber(int number) {
 		switch (number) {
+		case 0: {
+			return " không ";
+		}
 		case 1: {
-			return "mot";
+			return " một ";
 		}
 		case 2: {
-			return "hai";
+			return " hai ";
 		}
 		case 3: {
-			return "ba";
+			return " ba ";
 		}
 		case 4: {
-			return "bon";
+			return " bốn ";
 		}
 		case 5: {
-			return "nam";
+			return " năm ";
 		}
 		case 6: {
-			return "sau";
+			return " sáu ";
 		}
 		case 7: {
-			return "bay";
+			return " bảy ";
 		}
 		case 8: {
-			return "tam";
+			return " tám ";
 		}
 		case 9: {
-			return "chin";
+			return " chín ";
+		}
+		case 10: {
+			return " mười ";
 		}
 		default:
 			throw new IllegalArgumentException("Unexpected value: " + number);
@@ -64,10 +76,20 @@ public class Ex04 {
 		if(x == 0) {
 			return a = oneNumber(number);
 		}
-		if(x!=0 && y == 0) {
-			return a = oneNumber(x) + " muoi ";
+		if(x > 1 && y == 0) {
+			return a = oneNumber(x) + " mươi ";
+		}if(x == 1) {
+			if(y != 0) {
+				return a = oneNumber(10) + oneNumber(y);
+			}
+			if(y == 0) {
+				return a = oneNumber(10);
+			}
 		}
-		return a = oneNumber(x) + " muoi " + oneNumber(y);
+		if(x > 1) {
+			return a = oneNumber(x) + " mươi " + oneNumber(y);
+		}
+		return a;
 	}
 	private static String read3Number(int number) {
 		String a = "";
@@ -75,24 +97,62 @@ public class Ex04 {
 		int y = number/10 % 10;
 		int z = number - x*100 - y*10;
 		if(x == 0) {
-			return a = read2Number(number);
+			return a =  read2Number(number);
 		}
 		else if(x != 0) {
 			if(y == 0 && z != 0) {
-			return a = oneNumber(x) + " tram le " +oneNumber(z);
+			return a = oneNumber(x) + " trăm lẻ " +oneNumber(z);
 		}
-			if(y == 0 && z!=0) {
-				return a = oneNumber(x) + " tram ";
+			if(y == 0 && z==0) {
+				return a = oneNumber(x) + " trăm ";
 			}
 			if(y !=0 && z ==0) {
-				return a = oneNumber(x) + " tram " + oneNumber(y) + " muoi"; 
+				return a = oneNumber(x) + " trăm " + oneNumber(y) + " mười"; 
 			}
 			if(y != 0 && z!=0 ) {
-				return a = oneNumber(x) + " tram " + oneNumber(y) + " muoi " + oneNumber(z); 
+				return a = oneNumber(x) + " trăm " + oneNumber(y) + " mươi " + oneNumber(z); 
 			}
 		
 	}
 		return a;
 	
 }
+	private static String read6Num(int number) {
+		String s = String.valueOf(number);
+		String n = s.substring(0, s.length() - 6);
+		String t = s.substring(s.length() - 6, s.length() - 3);
+		String q = s.substring(s.length() - 3, s.length());
+		int x = Integer.parseInt(n);
+		int y = Integer.parseInt(t);
+		int z =Integer.parseInt(q);
+		if(x == 0) {
+			return read4Num(number);
+		}
+		if(y == 0) {
+			if(z == 0) {
+				return read3Number(x) + " triệu ";
+			}
+			if(z != 0) {
+				return read3Number(x) + " triệu " + read3Number(z);
+			}
+		}else if(z == 0) {
+			return read3Number(x) + read3Number(y) ;
+		}else {
+			return read3Number(x) + " triệu " + read3Number(y) + " nghìn " + read3Number(z);
+		}
+		
+		return read3Number(x) + " nghìn " + read3Number(y);
+	}
+	private static String read4Num(int number) {
+		String s = String.valueOf(number);
+		String n = s.substring(0, s.length() - 3);
+		String t = s.substring(s.length() - 3, s.length());
+		int x = Integer.parseInt(n);
+		int y = Integer.parseInt(t);
+		if(y == 0) {
+			return read3Number(x) + " nghìn " ;
+		}
+		return read3Number(x) + " nghìn " + read3Number(y);
+	}
+	
 }
