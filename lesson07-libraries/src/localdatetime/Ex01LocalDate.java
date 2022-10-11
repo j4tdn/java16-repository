644c1edba153dd5a4183f9861da609b03f1d4b62
DUK <duongminhduc@gmail.com>
2022-10-11 19:28:14 +0700

    lesson07-libraries 11.10.2022

--- lesson07-libraries/src/localdatetime/Ex01LocalDate.java
+++ lesson07-libraries/src/localdatetime/Ex01LocalDate.java
@@ -6,14 +6,14 @@ import java.time.format.DateTimeFormatter;
 public class Ex01LocalDate {
 	private static final String PATTERN = "dd/MM/yyyy E";
 	public static void main(String[] args) {
-        LocalDate now = LocalDate.now();  //láº¥y thá»�i gian hiá»‡n táº¡i
+        LocalDate now = LocalDate.now(); // Lấy ra thời gian hiện tại
         
         System.out.println("time now: " + now);
         
        DateTimeFormatter df = DateTimeFormatter.ofPattern(PATTERN);
        String text = df.format(now);
        System.out.println("formatted date: " + text);
-		
+		// 
 	}
 	//1
 }
