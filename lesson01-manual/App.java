a5d5aafff3af4c34b4c30f1caaa47b9eb3d896f6
nguyenthithuhien2272000 <105799375+nguyenthithuhien2272000@users.noreply.github.com>
2022-10-15 23:20:14 +0700

    Delete lesson01-manual directory

--- lesson01-manual/App.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDate;
-
-public class App { 
- public static void main(String[] args) {
-    System.out.println("welcome to  JAVA16 class");
-    System.out.println("publish date: " + LocalDate.now());
- }
-}
