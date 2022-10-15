998679311cf28e51653392b23eb26e060ba947b4
nguyenthithuhien2272000 <105799375+nguyenthithuhien2272000@users.noreply.github.com>
2022-10-15 23:21:54 +0700

    Delete lesson04-oop-features directory

--- lesson04-oop-features/src/encapsulation/inside/Company.java
+++
@@ -1,8 +0,0 @@
-package encapsulation.inside;
-
-public class Company {
-	public String name;
-	//protected int numberOfEmpls;
-    int numberOfEmpls;
-	private String policy;
-}
--- lesson04-oop-features/src/inheritance/fclass/InheritanceChild.java
+++
@@ -1,7 +0,0 @@
-package inheritance.fclass;
-
-public class InheritanceChild extends InheritanceParent {
-	void playingGame() {
-		System.out.println("Play online game");
-	}
-}
