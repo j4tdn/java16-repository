03c593674f7196a0d40fc5a1fed7340ff9cea52c
DUK <duongminhduc@gmail.com>
2022-10-17 21:27:20 +0700

    lesson09-duongminhduc 17.10.2022

--- lesson09-duongminhduc/src/de02/Ex03.java
+++ lesson09-duongminhduc/src/de02/Ex03.java
@@ -1,7 +1,16 @@
 package de02;
 
+import java.util.Scanner;
+
 public class Ex03 {
 	public static void main(String[] args) {
 
+		String s =  "xyz";
+		
+		
+	}
+	public static void permutation(String s)
+	{
+		
 	}
 }
