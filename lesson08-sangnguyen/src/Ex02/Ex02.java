c3d19631d432737d66cf87ab3ef8ed5ccf58e985
JOB <nhatsangnguyen02@gmail.com>
2022-10-19 00:18:59 +0700

    lesson08-sangnguyen 18.10.2022

--- lesson08-sangnguyen/src/Ex02/Ex02.java
+++ lesson08-sangnguyen/src/Ex02/Ex02.java
@@ -1,5 +1,66 @@
 package Ex02;
 
-public class Ex02 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+import javax.xml.transform.Templates;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		
+		String s = "";
+		System.out.println("nhap chuoi: ");
+		Scanner ip = new Scanner(System.in);
+		s = ip.nextLine();
+				
+		while (true) { 
+			if (s.matches("[[a-zA-Z]\\s]+")) {
+				System.out.println(normalize(s));
+				break;
+			} else {
+				System.out.println("vui long nhap dung cu phap [a-zA-Z]: ");
+				s = ip.nextLine();
+			}
+		}
+		
+	}	
+	public static boolean isLower(String s) {
+		String temp = s.toLowerCase();
+		if (s.equals(temp)) {
+			return true;
+		} else {
+		return false;
+		}
+	}
+	
+	public static boolean isUpper(String s) {
+		String temp = s.toUpperCase();
+		if (s.equals(temp)) {
+			return true;
+		} else {
+		return false;
+		}
+	}
+	
+	public static String normalize(String s) {
+		
+		
+		s = s.replaceAll("\\s+", " ").trim();
+		String[] strings = s.split("");
+		String string = strings[0].toUpperCase();
+		for (int i = 1; i < strings.length; i++) {
+			if (strings[i-1].equals(" ")) {
+				if (isLower(strings[i])) {
+					strings[i] = strings[i].toUpperCase();
+				}
+			} else {
+				if (isUpper(strings[i])) {
+					strings[i] = strings[i].toLowerCase();
+				}
+			}
+			string += strings[i];
+		}
+		return string;
+	}
+	
 }
