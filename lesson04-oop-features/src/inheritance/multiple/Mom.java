1241eadd0457cf1110f8cd8700bdf5e5c380a986
QuocThinhTran <105780360+QuocThinhTran@users.noreply.github.com>
2022-10-18 21:48:10 +0700

    Delete lesson04-oop-features/src directory

--- lesson04-oop-features/src/inheritance/multiple/Mom.java
+++
@@ -1,8 +0,0 @@
-package inheritance.multiple;
-
-public class Mom {
-		void coding() {
-			System.out.println("Mom --> coding");
-		}
-
-}
--- lesson04-oop-features/src/polymorphism/method/Ex02OverrdingParent.java
+++
@@ -1,9 +0,0 @@
-package polymorphism.method;
-
-public class Ex02OverrdingParent {
-	String text = "Hello";
-	
-	void log() {
-		System.out.println("Parent --> log...");
-	}
-}
