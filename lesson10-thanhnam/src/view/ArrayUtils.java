63d8dcb393b10d4b78795f98cbb156ca7563a74c
NguyenNam <nvtn41011002@gmail.com>
2022-11-12 23:46:05 +0700

    lesson10-baitap

---
+++ lesson10-thanhnam/src/view/ArrayUtils.java
@@ -0,0 +1,22 @@
+package view;
+
+
+import java.util.Arrays;
+
+public class ArrayUtils {
+	private void ArrayUtils() {
+		
+	}
+	
+	public static void printf(String prefix, int[] input) {
+		System.out.println(prefix + " --> " +Arrays.toString(input));
+	}
+	
+	public static void printf(String prefix, String[] input) {
+		System.out.println(prefix + " --> " +Arrays.toString(input));
+	}
+	
+	public static void printf(String prefix, Item[] input) {
+		System.out.println(prefix + " --> " +Arrays.toString(input));
+	}
+}
