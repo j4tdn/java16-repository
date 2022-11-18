a88c548479e2814ca525c240b58155f8cbeae933
jasmineChamCham <jasminebkdn@gmail.com>
2022-11-18 14:50:18 +0700

    lesson10-array 18.11.2022

---
+++ lesson10-array/src/generic/SortDemo.java
@@ -0,0 +1,21 @@
+package generic;
+
+import java.util.Arrays;
+
+import common.Tuple;
+
+public class SortDemo {
+	public static void main(String[] args) {
+		String[] sequence = { "A", "B" };
+		Arrays.sort(sequence);
+		System.out.println(Arrays.toString(sequence));
+
+		Tuple[] tuples = { new Tuple(1, 19), new Tuple(2, 20) };
+		Arrays.sort(tuples);
+		System.out.println(Arrays.toString(tuples));
+	}
+	
+	private static <E extends Comparable<E>> void sort(E[] o) {
+		Arrays.sort(o);
+	}
+}
