a26eb874b5197387cc1f520eb87b3cea8b22b6dd
= <linh1862002@gmail.com>
2022-11-24 20:30:34 +0700

    b1 24.11.2022

---
+++ lesson10-duclinh/src/Ex01/RemoveDuplicate.java
@@ -0,0 +1,12 @@
+package Ex01;
+
+public class RemoveDuplicate {
+	public static void main(String[] args) {
+		int [] array = {1,2,3,4,3,1};
+		
+	}
+	private static int remove(int a[]) {
+	
+		return 0;
+	}
+}
