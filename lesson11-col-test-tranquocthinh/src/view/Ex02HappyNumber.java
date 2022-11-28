c856a3c9f8d3420e134c63f79017df8fd6414bae
quocthinh <quocthinhtran2611@gmail.com>
2022-11-28 21:55:45 +0700

    lesson11-col-test-tranquocthinh

---
+++ lesson11-col-test-tranquocthinh/src/view/Ex02HappyNumber.java
@@ -0,0 +1,41 @@
+package view;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Ex02HappyNumber {
+	public static void main(String[] args) {
+		int n = 4;
+		if (isHappyNumber(n)) {
+			System.out.println(n + " is happy number ");
+		} else {
+			System.out.println(n + " is not happy number ");
+
+		}
+
+	}
+
+	private static int squareNumber(int n) {
+		int sumSquareNumber = 0;
+		while (n != 0) {
+			sumSquareNumber += (n % 10) * (n % 10);
+			n /= 10;
+		}
+		return sumSquareNumber;
+	}
+
+	private static boolean isHappyNumber(int n) {
+		Set<Integer> set = new HashSet<>();
+		while (true) {
+			n = squareNumber(n);
+			if (n == 1) {
+				return true;
+			}
+			if (set.contains(n)) {
+				return false;
+			}
+			set.add(n);
+		}
+	}
+
+}
