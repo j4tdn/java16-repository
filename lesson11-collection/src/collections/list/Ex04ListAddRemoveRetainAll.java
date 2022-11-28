38304c6ea6daff507bc9aede346cee43b8ecba62
= <linh1862002@gmail.com>
2022-11-28 20:42:33 +0700

    28.11.2022

---
+++ lesson11-collection/src/collections/list/Ex04ListAddRemoveRetainAll.java
@@ -0,0 +1,38 @@
+package collections.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex04ListAddRemoveRetainAll {
+	public static void main(String[] args) {
+		List<String> list1 = new ArrayList<>();
+		list1.add("a");
+		list1.add("b");
+		list1.add("c");
+		
+		List<String> list2 = new ArrayList<>();
+		list2.add("a");
+		list2.add("b");
+		list2.add("F");
+		
+		List<String> listAddAll = new ArrayList<>(list1);	
+		listAddAll.addAll(list2); // coppy hợp
+		printf("Hơp", listAddAll);
+			
+		List<String> listRemoveAll = new ArrayList<>(list1);	
+		listRemoveAll.removeAll(list2); // coppy hieu
+		printf("Hieu", listRemoveAll);
+		
+		List<String> listRetainAll = new ArrayList<>(list1);	
+		listRetainAll.retainAll(list2); // coppy Retain 
+		printf("Giao", listRetainAll);
+	}
+	private static void printf(String prefix,List<String> elements) {
+		System.out.print(prefix+"------->");
+		for(String element: elements) {
+			System.out.print(element + " ");
+		}
+		System.out.println();
+	}
+	
+}
