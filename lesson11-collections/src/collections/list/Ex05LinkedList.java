1e197e3dd57d0870e4ef5292359a00f35e63b1c3
tthinh <nguyenminhthethinh@gmail.com>
2022-11-28 20:41:29 +0700

    lesson01-collections 28.11.2022

---
+++ lesson11-collections/src/collections/list/Ex05LinkedList.java
@@ -0,0 +1,19 @@
+package collections.list;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Ex05LinkedList {
+	public static void main(String[] args) {
+		// declare -->
+		// compine --> using same function from List<T>
+		List<Integer> digits = new ArrayList<>();
+		List<Integer> numbers = new LinkedList<>();
+		
+		// runtime --> call to implementation function
+		// from ArrayList, LinkedList
+		
+		// ==> different handing time
+	}
+}
