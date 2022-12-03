cab86a102f18f56841bb4d11a3e2776dde64eb6a
thanhnhat <lethanhnhat2010@gmail.com>
2022-12-03 21:09:50 +0700

    lesson11-collection 28.11.2022

--- lesson11-collections/src/collections/set/Ex02SetInside.java
+++ lesson11-collections/src/collections/set/Ex02SetInside.java
@@ -1,5 +1,7 @@
 package collections.set;
 
+import static java.util.Comparator.*;
+
 import java.util.Comparator;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -10,34 +12,21 @@ import bean.Employee;
 public class Ex02SetInside {
 	public static void main(String[] args) {
 
-		Comparator<Employee> cmp = comparing(Employee::getName, reverseOrder().thenComparing(Employee::getSalary));
+		Comparator<Employee> cmp = comparing(Employee::getName, reverseOrder())
+				.thenComparing(Employee::getSalary);
 
 		Set<Employee> company = new TreeSet<>(cmp);
 		company.add(new Employee(1, "E1", 100d));
 		company.add(new Employee(2, "E2", 200d));
 		company.add(new Employee(3, "E3", 300d));
-
-		company.add(new Employee(4, "E3", 300d));
-
-		System.out.println("size: " + company.size());
+		company.add(new Employee(4, "E3", 200d));
+System.out.println("size: " + company.size());
+		printf(company);
 	}
 
-	private static Employee comparing(Set<Employee> ) {
-		// sort desc by name, asc by salary
-
-		Comparator cmp = new Comparator<Employee>() {
-			@Override
-			public int compare(Employee e1, Employee e2) {
-				int sortByName = e1.getName().compareTo(e2.getName());
-				if (sortByName < 0) {
-					return 1;
-				}
-				if (sortByName == 0) {
-					return -1;
-				}
-				return e1.getSalary().compareTo(e2.getSalary());
-			}
-		};
+	private static <E> void printf(Set<E> set) {
+		for (E e : set) {
+			System.out.println(e);
+		}
 	}
-	
-	}
+}
