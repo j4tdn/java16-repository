b5e75174355e5604b8a3dd48c215278b90c2fcf9
buivu <buianhvu226@gmail.com>
2022-12-04 13:47:56 +0700

    lesson11-test-28.11.2022

--- lesson11-col-tests-buianhvu/src/ex01MissIT/Demo.java
+++ lesson11-col-tests-buianhvu/src/ex01MissIT/Demo.java
@@ -1,5 +1,17 @@
 package ex01MissIT;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo {
 
+	public static void main(String[] args) {
+		List <Contestants> miss = new ArrayList<>();
+		
+		Contestants miss1 = new Contestants(2, 18, 1, 0, 1);
+		Contestants miss2 = new Contestants(2, 10, 2, 1, 1);
+		Contestants miss3 = new Contestants(2, 18, 1, 2, 4);
+		
+	}
+	
 }
