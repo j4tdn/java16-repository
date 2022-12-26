e4ea28665e2fa4f87989516f96267ba9f882213a
quangdung <dungdixa3@gmail.com>
2022-12-26 15:19:54 +0700

    lesson11-collections 26.12.2022

---
+++ lesson11-collections/src/collections/list/Ex05LList.java
@@ -0,0 +1,19 @@
+package collections.list;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Ex05LList {
+	public static void main(String[] args) {
+		// declare -->
+		// compile --> using same functions from List<T>
+		List<Integer> digits = new ArrayList<>();
+		List<Integer> numbers = new LinkedList<>();
+		
+		// runtime --> call to implementation function
+		// from ArrayList, LinkedList
+		
+		// ==> different handling time
+	}
+}
---
+++ lesson11-collections/src/collections/set/Ex01BasicSetDemo.java
@@ -0,0 +1,31 @@
+package collections.set;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Ex01BasicSetDemo {
+	
+	public static void main(String[] args) {
+		// List --> ArrayList, LinkedList --> array
+		// get(i)
+		
+		Set<String> text = new TreeSet<>(Comparator.reverseOrder());
+		text.add("123456789");
+		text.add("zzzzabc");
+		text.add("abcccc");
+		
+		text.add(new String("b"));
+		
+		System.out.println("size: " + text.size());
+		printf(text);
+	}
+	
+	private static <E> void printf(Set<E> set) {
+		for (E e: set) {
+			System.out.println(e);
+		}
+	}
+}
