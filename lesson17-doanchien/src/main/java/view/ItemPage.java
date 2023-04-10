69c300d5e8215d125e97352b494532b40ccc0200
doan_chien <chienvandoan9116@gmail.com>
2023-04-10 21:32:13 +0700

    lesson17-doanchien 10.04.2023

--- lesson17-doanchien/src/main/java/view/ItemPage.java
+++ lesson17-doanchien/src/main/java/view/ItemPage.java
@@ -1,17 +1,59 @@
 package view;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 import service.ItemService;
 import service.ItemServiceImpl;
 import utils.PrintUtils;
 
 public class ItemPage {
-private static ItemService itemService;
+	
+	private static Connection conn = null;
+	
+	public static Connection getConnection() {
+		String DRIVER = "com.mysql.cj.jdbc.Driver";
+		String URL = "jdbc:mysql://localhost:3306/java16_shopping_test";
+		String USER = "root";
+		String PASS = "Ashinone";
+		if (conn == null) {
+			try {
+				Class.forName(DRIVER);
+				conn = DriverManager.getConnection(URL, USER, PASS);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return conn;
+	}
+	private static ItemService itemService;
 	
 	static {
 		itemService = new ItemServiceImpl();
 	}
+	
+	public static void ex01() {
+		
+		try {
+			LocalDate sellDate = LocalDate.of(2019, 11, 23);
+			String sql = "SELECT * FROM sales WHERE ThoiGianBan = ?";
+			PreparedStatement pstmt;
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setDate(1, java.sql.Date.valueOf(sellDate));
+			ResultSet rs = pstmt.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 	public static void main(String[] args) {
-		PrintUtils.generate("GET ALL ITEM ", itemService.getAll());
-		System.out.println("=======================================");
+		ex01();
+		
+		
 	}
 }
