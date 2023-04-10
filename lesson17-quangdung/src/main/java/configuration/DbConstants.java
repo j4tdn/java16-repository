8dbed92a8a421ad36ea3e99c50efde9b5029f062
quangdung <dungdixa3@gmail.com>
2023-04-10 21:28:46 +0700

    10.04.2023 quangdung

---
+++ lesson17-quangdung/src/main/java/configuration/DbConstants.java
@@ -0,0 +1,13 @@
+package configuration;
+
+public class DbConstants {
+	
+	private DbConstants() {
+		
+	}
+	public static final String APPLICATION_PROPERTIES = "db-config.properties";
+	public static final String DRIVER= "DRIVER";
+	public static final String URL = "URL";
+	public static final String USER = "USER";
+	public static final String PASS = "PASS";
+}
