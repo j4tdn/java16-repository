0a6f1057f567757c33c2a1749f7fc9ea881bb6c7
JOB <nhatsangnguyen02@gmail.com>
2023-05-14 15:12:07 +0700

    lesson18-exam-1.0.6 14.05.2023

--- lesson18-sangnguyen/lesson18-sangnguyen.sql
+++ lesson18-sangnguyen/lesson18-sangnguyen.sql
@@ -1,3 +1,4 @@
+-- ----------- DDL ----------- --								
 CREATE SCHEMA SCHOOL;
 USE SCHOOL;
 CREATE TABLE STUDENT (
@@ -15,7 +16,7 @@ CREATE TABLE CLASS (
 
 CREATE TABLE RESULT (
 	STUDENT_ID	INT			NOT NULL,
-    `SUBJECT`	VARCHAR(5)	NOT	NULL,
+    `SUBJECT`	VARCHAR(50)	NOT	NULL,
     SCORE		FLOAT		NOT NULL			
 );
 
@@ -32,3 +33,57 @@ REFERENCES STUDENT(ID);
 ALTER TABLE RESULT
 ADD CONSTRAINT PK_RESULT
 PRIMARY KEY (STUDENT_ID, `SUBJECT`);
+
+-- -------------- DML -------------- --
+
+INSERT INTO CLASS (ID, `NAME`, TEACHER)
+VALUES	(1, "12A", "Ho Cong Trung"),
+		(2, "12B", "Dang Tu Anh"),
+        (3, "12C", "Nguyen Van Tam");
+
+INSERT INTO STUDENT (ID, `NAME`, GENDER, CLASS_ID) 
+VALUES 	(1,	 "Dinh Thi Ngoc", 		"Female", 	2),
+		(2,	 "Nguyen Thanh Hung",	"Male", 	1),
+        (3,	 "Tran Mai Hoa", 		"Female", 	2),
+        (4,	 "Doan Quang Vinh",		"Male", 	1),
+        (5,	 "Cao Anh Dao", 		"Female", 	3),
+        (6,	 "Tran Kim Tuyen", 		"Male", 	3);
+        
+INSERT INTO RESULT	(STUDENT_ID, `SUBJECT`, SCORE)
+VALUES	(1, "Math", 8),
+		(2, "Literature", 7),
+        (3, "History", 9.5),
+        (4, "Math", 6.8),
+        (5, "Literature", 4.9),
+        (6, "History", 8.2),
+        (3, "Math", 9.8),
+        (1, "Literature", 8.2),
+        (4, "History", 8.8);
+        
+-- ---------- Answer Question 1 ------------ --
+-- Khoá chính của bảng RESULT là 2 cột STUDENT_ID và SUBJECT --
+
+-- ---------- Write 3 Questions ------------ --
+-- 1 --
+SELECT 	st.`NAME`, 
+		st.GENDER,
+        cl.TEACHER
+  FROM STUDENT st
+  JOIN CLASS cl
+	ON st.CLASS_ID = cl.id;
+
+-- 2 --
+SElECT 	cl.TEACHER,
+		cl.`NAME`,
+        st.ID
+  FROM CLASS cl
+  JOIN STUDENT st
+    ON cl.ID = st.CLASS_ID
+ WHERE cl.`NAME` = "12C";
+ 
+ -- 3 --
+ SELECT	rs.`SUBJECT`,
+		rs.STUDENT_ID
+   FROM RESULT rs
+  WHERE (rs.`SUBJECT` = 'Math' OR rs.`SUBJECT` = 'Literature') AND (rs.SCORE >= 8);	
+
